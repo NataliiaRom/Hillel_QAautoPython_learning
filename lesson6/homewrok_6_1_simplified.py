fba4166a1005a547f73dae4a18e80fe7d755c00c
NataliiaRom <nataliarom88@gmail.com>
2025-07-08 23:49:20 +0200

    made homework_6_1 simplier

---
+++ lesson6/homewrok_6_1_simplified.py
@@ -0,0 +1,29 @@
+user_string = input("Enter any string here: ")
+check_value = 10
+
+unique_symbols = []
+symbols_count = []
+for i in user_string:
+    if i != " " and i not in unique_symbols:
+        unique_symbols.append(i)
+        count_i = user_string.count(i)
+        symbols_count.append(count_i)
+
+found = False
+for j in symbols_count:
+    if j >= check_value:
+        found = True
+        print(True)
+        break
+
+if not found:
+    print(False)
+
+# symbols_count_string = ",".join(symbols_count)
+# print(symbols_count)
+# print(symbols_count_string)
+
+
+
+# print(check_value in symbols_count)
+
