3595a514085a6082676f533548a67acff314eb36
NataliiaRom <nataliarom88@gmail.com>
2025-07-10 11:59:27 +0200

    completed homewrok_6_3

---
+++ lesson6/homework_6_3.py
@@ -0,0 +1,16 @@
+lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum']
+
+string_list = []
+for i in lst1:
+
+##### Version with isinstance() #######
+    if isinstance(i, str):
+        string_list.append(i)
+
+##### Version with .__name__ #######
+    if type(i).__name__ == 'str':
+        string_list.append(i)
+    else:
+        continue
+
+print(string_list)
