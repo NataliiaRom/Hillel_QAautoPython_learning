74b5ea0b2ea8bbf393d77bc214a4affb1f86d07a
NataliiaRom <nataliarom88@gmail.com>
2025-07-18 12:19:48 +0200

    some changes and improvements to homework_8_1. more precise try-except

--- lesson8/homework_8_1.py
+++ lesson8/homework_8_1.py
@@ -1,4 +1,4 @@
-list1 = ["1, 2, 3, 4", "1, 2, 3, 4, 50", "1, 2, 3a"]
+list1 = ["1, 2, s3, 4", "1, 2, 3, 4, 50", "1, 2a, 3"]
 
 
 def list_elts_sum(list):
@@ -6,12 +6,16 @@ def list_elts_sum(list):
     for i in list:
         i_str_as_list = i.split(',')
         i_list_of_ints = []
-        try:
-            for j in i_str_as_list:
+        for j in i_str_as_list:
+            try:
                 i_list_of_ints.append(int(j.strip()))
+            except ValueError as ve:
+                i_list_of_ints.append(None)
+                break
+        try:
             suma.append(sum(i_list_of_ints))
-        except Exception as esc:
-            suma.append(f"{type(esc).__name__}: Cannot do this")
+        except TypeError as te:
+            suma.append(f"{type(te).__name__}: Cannot do this")
 
     return suma
 
