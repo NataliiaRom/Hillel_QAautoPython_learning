dd72a86a2444f6647b5d6336c5bee0fdddc22896
NataliiaRom <nataliarom88@gmail.com>
2025-08-01 17:31:16 +0200

    small changes to the completed homework

--- tests/lesson11_tests/test_homework11_ver1.py
+++ tests/lesson11_tests/test_homework11_ver1.py
@@ -10,7 +10,7 @@ def clear_logfile_input(file_path):
 def user_login_events_handler(file_path,name,user_status):
     with open(log_file) as log:
         lines = log.readlines()  # to get an output as a list of lines
-        # checking log info correspondence with the last line only
+        # checking log info corresponds the last line only
         assert lines[-1].strip().endswith(f'Login event - Username: {name}, Status: {user_status}'), \
             f"There is no event for {name} with {user_status} login. Test failed"
         print(f"log includes info for {name} with {user_status} login")
@@ -19,19 +19,19 @@ def user_login_events_handler(file_path,name,user_status):
 class TestUserLogin:
 
     def test_successful_login(self):
-        # clear_logfile_input(log_file)  # not necessary, because I check the correspondence with the last line only
+        # clear_logfile_input(log_file)  # not necessary, because I check the correspondence to the last line only
         status = "success"
         log_event(user,status)
         user_login_events_handler(log_file,user,status)
 
     def test_expired_login(self):
-        # clear_logfile_input(log_file)  # not necessary, because I check the correspondence with the last line only
+        # clear_logfile_input(log_file)  # not necessary, because I check the correspondence to the last line only
         status = "expired"
         log_event(user,status)
         user_login_events_handler(log_file,user,status)
 
     def test_weird_login(self):
-        # clear_logfile_input(log_file)  # not necessary, because I check the correspondence with the last line only
+        # clear_logfile_input(log_file)  # not necessary, because I check the correspondence to the last line only
         status = "lalala"
         log_event(user,status)
         user_login_events_handler(log_file,user,status)
